474ab4c3483969357f580456f63fa8fddc4bb966
AnnD0508 <3826347@mail.ru>
2022-12-12 01:41:33 +0300

    modified:   task_one.py 	modified:   task_two.py

--- Homework7/task_one.py
+++ Homework7/task_one.py
@@ -1,12 +1,12 @@
 def get_winners(results):
     results_dict = {}
-    
-    for winner_result in enumerate(results): 
-        results_dict[winner_result[0]+1]  = winner_result[1]
-   
+
+    for winner_result in enumerate(results):
+        results_dict[winner_result[0]+1] = winner_result[1]
+
     sorted_results_dict = sorted(results_dict, key=results_dict.get)
     return (list(reversed(sorted_results_dict))[:3])
-   
-#result = get_winners([115, 352, 0, 310, 500])
-result = get_winners([110, 450, 600, 310, 500])
+
+
+result = get_winners([115, 352, 0, 310, 500])
 print(result)
